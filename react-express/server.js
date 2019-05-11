f885021e07c5e15ea08d700e51f230712d6f7b6e
rargueta92 <rudy.argueta@aol.com>
2019-05-11 07:51:25 -0600

    updated yarn build with a server.js

---
+++ react-express/server.js
@@ -0,0 +1,34 @@
+
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+// mongodb connection link to the database
+
+const mongoose = require("mongoose");
+const mongoURL = process.env.PROD_MONGODB || "mongodb://rudya:Demons11@ds119585.mlab.com:19585/heroku_08hlwx15"
+mongoose.connect(mongoURL, {useNewUrlParser: true})
+  .then(() => {
+    console.log("🗄 ==> Successfully connected to mongoDB.");
+  })
+  .catch((err) => {
+    console.log(`Error connecting to mongoDB: ${err}`);
+  });
+
+require("./routes/api-routes")(app);
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
+
+
+
