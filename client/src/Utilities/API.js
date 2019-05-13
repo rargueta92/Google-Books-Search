d07e92fe7e760a1d91ac41909f50515f544ddcb2
rargueta92 <rudy.argueta@aol.com>
2019-05-12 21:37:50 -0600

    added some folders

---
+++ client/src/Utilities/API.js
@@ -0,0 +1,31 @@
+
+import axios from "axios";
+const SEARCH_URL = "https://www.googleapis.com/books/v1/volumes?q=";
+
+
+export default {
+
+  getGoogleBookSearch: function(query) {
+    return axios.get(SEARCH_URL + query);
+  },
+
+    // Saves a book to the database
+    saveBook: function(bookSaveChoice) {
+      return axios.post("/api/books", bookSaveChoice);
+    },
+  
+  // Gets all books
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
+  // Gets the book with the given id
+  getBook: function(id) {
+    return axios.get("/api/books/" + id);
+  },
+  // Deletes the book with the given id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
+  },
+
+
+};
