c74b08d2af09b322103bbfb35b1eec3a840f7a49
rargueta92 <rudy.argueta@aol.com>
2019-05-12 21:50:34 -0600

    added a footer and grid folder

---
+++ client/src/Components/Footer/index.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+
+    <footer className="footer mt-auto py-3">
+      <div className="container">
+        <span is="copyright" class="text-muted">&copy; Copyright 2019 Rudy Argueta</span>
+      </div>
+    </footer>
+
+  );
+}
+
+export default Footer;
